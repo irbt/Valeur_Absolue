482aa9a4ad8cb0157b4541ca462fc47af3dcfbb5
Emmanuel Marchal <81331382+irbt@users.noreply.github.com>
2022-11-24 21:34:29 +0100

    version finale

--- Program.cs
+++ Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+// See https://aka.ms/new-console-template for more information
+
+namespace exoCSharp
+{
+
+    class ValAbsTask
+    {
+        static int ValAbs (int number)
+        {
+            
+            return number>= 0 ? number : number * -1;
+        }
+
+        static void Main(string[] args)
+        {
+Console.WriteLine("Given an integer, write a method that returns its absolute value.");
+            Console.WriteLine(ValAbs(654));
+            Console.WriteLine(ValAbs(-54));
+            Console.WriteLine(ValAbs(0));
+            Console.WriteLine(ValAbs(-69046354));
+        }
+
+    }
+}
+
